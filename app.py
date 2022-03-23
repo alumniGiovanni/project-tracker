9a787d4c21541c5e8105015aed532c234a1485bf
Giovanni <giovanni.andrade9991@gmail.com>
2022-03-22 22:52:50 -0300

    added the addition of tasks skeleton

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,5 +6,17 @@ app = Flask(__name__)
 @app.route("/")
 def show_projects():
     return render_template("index.html")
+
+@app.route("projects/<projects_id>")
+def show_tasks(project_id):
+    return render_template("project-tasks.html", project_id=project_id)
+
+@app.route("/add/project", methods=['POST'])
+def add_project():
+    return "added"
+
+@app.route("/add/task/<project_id>", methods=['POST'])
+def add_project():
+    return "task added"
 
 app.run(debug=True, host="127.0.0.1", port=3000)
