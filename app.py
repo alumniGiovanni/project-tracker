4117b6e84f2eb470e9bf9f6ec5d33ce6ca9cb29f
Giovanni <giovanni.andrade9991@gmail.com>
2022-03-22 22:31:44 -0300

    Making adjustments to run on flask - btw im pissed on how it handles the render template

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def show_projects():
-    return "<p>teste</p>"
+    return render_template("index.html")
 
 app.run(debug=True, host="127.0.0.1", port=3000)
