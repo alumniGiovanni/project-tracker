4663ed2789c1de53695268569962ad83839e3297
Giovanni <giovanni.andrade9991@gmail.com>
2022-03-31 20:27:14 -0300

    Fixed all bugs and fed dummy data

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/project_tracker'
+app.config['SECRET_KEY'] = (r'\xa5\x9f\xe7o\xb9\xfe\x06\xcb\x9e\'4SX\xd0|\x8b:\xa8\xde\xb7\xc3F')
 @app.route("/")
 def show_projects():
     return render_template("index.html")
